7f8bdb38dc95a9a65085d738635688e7e0458a81
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-07-26 23:20:10 -0700

    add hover effects for game imgs

--- src/components/GameImg/index.js
+++ src/components/GameImg/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 
 function GameImg(props) {
     var styles = {
@@ -9,6 +10,7 @@ function GameImg(props) {
     }
     return (
         <img 
+            className="game-img"
             alt={props.name} 
             src={props.image} 
             style={styles} 
